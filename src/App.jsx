f5cfadd8f0c468cd189733d2e78819d1fe2038af
pdhende <pdhendework@gmail.com>
2025-08-29 14:14:08 -0400

    Updated input types and modified UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fromCurrency, setFromCurrency] = useState("");
+  const [toCurrency, setToCurrency] = useState("");
 
   return (
     <>
-    <div>
-       <h1>Currency Converter</h1>
+      <div>
+        <h1>Currency Converter</h1>
       </div>
       <div className="card">
-        <h2>From :</h2>
-        <input type="number" placeholder="Enter amount" />
+        <div classBorder="5px solid yellow">
+          <h2>From :</h2>
+          <select
+            id="currency"
+            value={fromCurrency}
+            onChange={(e) => setFromCurrency(e.target.value)}
+            className="border p-2 rounded"
+          >
+            <option value="Select">--Select--</option>
+            <option value="USD">United States Dollar</option>
+            <option value="INR">Indian Rupees</option>
+          </select>
+          <input type="number" placeholder="Enter Amount" />
+        </div>
+          <button>Convert</button>
+        <div>
         <h2>To :</h2>
-        <input type="number" placeholder="" />
-        <button onClick={() => setCount((count) => count + 1)}>
-          Convert
-        </button>
+        <select
+            id="currency"
+            value={toCurrency}
+            onChange={(e) => setToCurrency(e.target.value)}
+            className="border p-2 rounded"
+          >
+            <option value="Select">--Select--</option>
+            <option value="INR">Indian Rupees</option>
+            <option value="USD">United States Dollar</option>
+          </select>
+        <input type="number" placeholder="0" />
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
