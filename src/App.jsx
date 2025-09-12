1afcbb47483c176515f3b0dcc5d6e3380e42f0ac
pdhende <pdhendework@gmail.com>
2025-09-12 13:16:09 -0400

    Added logic to calculate the conversion rates

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,37 @@ import './App.css'
 function App () {
   const [fromCurrency, setFromCurrency] = useState('Select');
   const [toCurrency, setToCurrency] = useState('Select');
+  const [countryList, setCountryList] = useState([]);
   const [fromAmount, setFromAmount] = useState(0);
   const [toAmount, setToAmount] = useState(0);
-  const [ratesList, setRatesList] = useState(0);
 
-  function getExchangeRate(fromCurrency, toCurrency) {
-      // Fetch exchange rates
-      console.log("inside getExchangeRate");
-      if(fromCurrency && toCurrency !== 'Select') {
-      fetch('https://open.er-api.com/v6/latest/' + fromCurrency)
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all?fields=currencies,flags')
+      .then(response => response.json())
+      .then(data => {
+        setCountryList(data);
+      })
+  }, []);
+
+
+  const getConversionRate = async () => {
+    // This function gets the conversion rates and calculates the amount
+    try{
+    if (fromCurrency && toCurrency !== 'Select') {
+      await fetch('https://open.er-api.com/v6/latest/' + fromCurrency)
         .then(response => response.json())
         .then(data => {
-          setRatesList(data.rates)
-          console.log(data)
-        })}
+          const rate = (data.rates[toCurrency]);
+          console.log('Exchange Rate:', rate);
+          console.log('From Amount:', fromAmount);
+          const convertedAmount = (fromAmount * rate).toFixed(2);
+          setToAmount(convertedAmount);
+        })
+    }
+  }catch(error){
+    console.error('Error fetching exchange rate:', error);
   }
+}
 
   return (
     <>
@@ -34,8 +50,19 @@ function App () {
             onChange={e => setFromCurrency(e.target.value)}
           >
             <option value='Select'>--Select--</option>
-            <option value='USD'>United States Dollar</option>
-            <option value='INR'>Indian Rupees</option>
+            {countryList.map((country, index) => {
+              if (!country.currencies) return null
+
+              const entries = Object.entries(country.currencies)
+              if (entries.length === 0) return null
+
+              const [code, currency] = entries[0]
+              return (
+                <option key={index} value={code}>
+                  ({currency.symbol}) - {currency.name}
+                </option>
+              )
+            })}
           </select>
         </div>
         <div>
@@ -46,19 +73,33 @@ function App () {
             onChange={e => setToCurrency(e.target.value)}
           >
             <option value='Select'>--Select--</option>
-            <option value='INR'>Indian Rupees</option>
-            <option value='USD'>United States Dollar</option>
+            {countryList.map((country, index) => {
+              if (!country.currencies) return null
+
+              const entries = Object.entries(country.currencies)
+              if (entries.length === 0) return null
+
+              const [code, currency] = entries[0]
+              return (
+                <option key={index} value={code}>
+                  ({currency.symbol}) - {currency.name}
+                </option>
+              )
+            })}
           </select>
-          </div>
-        <div><input
+        </div>
+        <div>
+          <input
             type='number'
             placeholder='Enter Amount'
             value={fromAmount}
             onChange={e => setFromAmount(e.target.value)}
           />
-          <button onClick={() => getExchangeRate(fromCurrency, toCurrency)}>Convert</button>
-          </div>
-          <div>
+          <button onClick={() => getConversionRate()}>
+            Convert
+          </button>
+        </div>
+        <div>
           <input type='number' placeholder='0' value={toAmount} />
         </div>
         <a href='https://www.exchangerate-api.com'>
@@ -69,4 +110,4 @@ function App () {
   )
 }
 
-export default App;
+export default App
