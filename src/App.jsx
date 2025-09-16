17925e9f0c66a817797c32b0812134a8db3ec688
pdhende <pdhendework@gmail.com>
2025-09-16 14:07:57 -0400

    Updated logic to filter out the redundant currency values

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,56 @@ import { use, useEffect, useState } from 'react'
 import './App.css'
 
 function App () {
-  const [fromCurrency, setFromCurrency] = useState('Select');
-  const [toCurrency, setToCurrency] = useState('Select');
-  const [countryList, setCountryList] = useState([]);
-  const [fromAmount, setFromAmount] = useState(0);
-  const [toAmount, setToAmount] = useState(0);
+  const [fromCurrency, setFromCurrency] = useState('Select')
+  const [toCurrency, setToCurrency] = useState('Select')
+  const [countryList, setCountryList] = useState([])
+  const [fromAmount, setFromAmount] = useState(0)
+  const [toAmount, setToAmount] = useState(0)
 
   useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all?fields=currencies,flags')
+    fetch('https://restcountries.com/v3.1/all?fields=currencies')
       .then(response => response.json())
       .then(data => {
-        setCountryList(data);
+        const tempList = data
+          .map((country, index) => {
+            const entries = Object.entries(country.currencies)
+            if (entries.length === 0) return null
+            const [currencyCode, currency] = entries[0]
+            return {
+              currencyCode,
+              currencyName: currency.name,
+              currencySymbol: currency.symbol
+            }
+          })
+          .filter(Boolean); //this filter is to remove null values
+        
+          //Remove the redundant currency codes
+        const tempArr = new Set();
+        const filteredCurrency = tempList.filter(({ currencyCode }) => {
+          if (tempArr.has(currencyCode)) return false;
+          tempArr.add(currencyCode);
+          return true;
+        })
+        setCountryList(filteredCurrency);
       })
-  }, []);
-
+  }, [])
 
   const getConversionRate = async () => {
     // This function gets the conversion rates and calculates the amount
-    try{
-    if (fromCurrency && toCurrency !== 'Select') {
-      await fetch('https://open.er-api.com/v6/latest/' + fromCurrency)
-        .then(response => response.json())
-        .then(data => {
-          const rate = (data.rates[toCurrency]);
-          console.log('Exchange Rate:', rate);
-          console.log('From Amount:', fromAmount);
-          const convertedAmount = (fromAmount * rate).toFixed(2);
-          setToAmount(convertedAmount);
-        })
+    try {
+      if (fromCurrency && toCurrency !== 'Select') {
+        await fetch('https://open.er-api.com/v6/latest/' + fromCurrency)
+          .then(response => response.json())
+          .then(data => {
+            const rate = data.rates[toCurrency]
+            const convertedAmount = (fromAmount * rate).toFixed(2)
+            setToAmount(convertedAmount)
+          })
+      }
+    } catch (error) {
+      console.error('Error fetching exchange rate:', error)
     }
-  }catch(error){
-    console.error('Error fetching exchange rate:', error);
   }
-}
 
   return (
     <>
@@ -50,16 +67,10 @@ function App () {
             onChange={e => setFromCurrency(e.target.value)}
           >
             <option value='Select'>--Select--</option>
-            {countryList.map((country, index) => {
-              if (!country.currencies) return null
-
-              const entries = Object.entries(country.currencies)
-              if (entries.length === 0) return null
-
-              const [code, currency] = entries[0]
+            {countryList.map((currency, index) => {
               return (
-                <option key={index} value={code}>
-                  ({currency.symbol}) - {currency.name}
+                <option key={index} value={currency.currencyCode}>
+                  ({currency.currencySymbol}) - {currency.currencyName}
                 </option>
               )
             })}
@@ -73,16 +84,10 @@ function App () {
             onChange={e => setToCurrency(e.target.value)}
           >
             <option value='Select'>--Select--</option>
-            {countryList.map((country, index) => {
-              if (!country.currencies) return null
-
-              const entries = Object.entries(country.currencies)
-              if (entries.length === 0) return null
-
-              const [code, currency] = entries[0]
+             {countryList.map((currency, index) => {
               return (
-                <option key={index} value={code}>
-                  ({currency.symbol}) - {currency.name}
+                <option key={index} value={currency.currencyCode}>
+                  ({currency.currencySymbol}) - {currency.currencyName}
                 </option>
               )
             })}
@@ -95,9 +100,7 @@ function App () {
             value={fromAmount}
             onChange={e => setFromAmount(e.target.value)}
           />
-          <button onClick={() => getConversionRate()}>
-            Convert
-          </button>
+          <button onClick={() => getConversionRate()}>Convert</button>
         </div>
         <div>
           <input type='number' placeholder='0' value={toAmount} />
