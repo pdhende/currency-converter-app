fc07ecb4850523822e6d147dc85a39abd59188cf
pdhende <pdhendework@gmail.com>
2025-09-17 22:23:15 -0400

    Modified UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cardColor: "#CCAFA5",
+        btnColor:"#EEEDE7",
+      },
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
